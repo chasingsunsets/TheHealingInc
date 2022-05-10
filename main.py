08e0418e84981b29068f3ebed3158f116204dd7d
Lnnatthida <96171907+Lnnatthida@users.noreply.github.com>
2022-05-10 18:38:01 +0800

    create base template

--- main.py
+++
@@ -1,3 +0,0 @@
-# TheHealingInc
-
-print('Hello World')
---
+++ routes/main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	const title = 'Video Jotter';
+	// renders views/index.handlebars, passing title as an object
+	res.render('index', { title: title })
+});
+
+router.get('/catalogue', (req, res) => {
+	res.render('catalogue');
+});
+
+module.exports = router;
