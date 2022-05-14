c34f5816000a2d5a100a8235b458f7d9f8eafd7b
meilingmz <limmeiling108@gmail.com>
2022-05-15 01:02:50 +0800

    login, register +broken flash
    delete node_modules>npm install>npm install connect-flash flash-messenger

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const flashMessage = require('../helpers/messenger');
 
 router.get('/', (req, res) => {
 	const title = 'Video Jotter';
@@ -10,5 +11,25 @@ router.get('/', (req, res) => {
 router.get('/catalogue', (req, res) => {
 	res.render('catalogue');
 });
+
+
+
+
+
+
+// router.post('/flash', (req, res) => {
+// 	const message = 'This is an important message';
+// 	const error = 'This is an error message';
+// 	const error2 = 'This is the second error message';
+// 	// req.flash('message', message);
+// 	// req.flash('error', error);
+// 	// req.flash('error', error2);
+// 	flashMessage(res, 'success', message);
+// 	flashMessage(res, 'info', message);
+// 	flashMessage(res, 'error', error);
+// 	flashMessage(res, 'error', error2, 'fas fa-sign-in-alt', true);
+// 	res.redirect('/landing');
+// });
+
 
 module.exports = router;
