e822167ef03970e056f7706c310234dce390b937
captaiN455 <q2467231710@gmail.com>
2022-06-24 17:18:29 +0800

    edit 1.0

--- routes/cart.js
+++ routes/cart.js
@@ -5,20 +5,22 @@ const Order = require('../models/Order');
 const bcrypt = require('bcryptjs');
 const moment = require('moment');
 const User = require('../models/User');
+const ensureAuthenticated = require('../helpers/auth');
+// const { where } = require('sequelize/types');
 
 
 router.get('/cart', (req, res) => {
 	res.render('../views/cart/cart.handlebars');
 });
 
-router.post('/addorder', (req, res) => {
+router.post('/addorder', ensureAuthenticated, (req, res) => {
 	let custno = req.user.custno;
 	let product = req.body.product;
 	let amount = req.body.amount;
 	let price = req.body.price;
 
 	Order.create(
-		{custno, product, amount, price}
+		{ custno, product, amount, price }
 	)
 		.then((order) => {
 			console.log(order.toJSON());
@@ -27,19 +29,43 @@ router.post('/addorder', (req, res) => {
 		.catch(err => console.log(err));
 });
 
-router.get('/listOrder', (req, res) => {
+router.get('/listOrder', ensureAuthenticated, (req, res) => {
 	Order.findall({
-		where: { custno: req.user.custno},
+		where: { custno: req.user.custno },
 		order: [['database', 'DESC']],
 		raw: true
 	})
 		.then((orders) => {
 			// pass object to cart.handlebars
-			res.render('../views/cart/cart.handlebars', {orders});
+			res.render('../views/cart/cart.handlebars', { orders });
 		})
 		.catch(err => console.log(err));
 });
 
+router.get('/editOrder/:id', ensureAuthenticated, (req, res) => {
+	Video.findByPk(req.params.id)
+		.then((order) => {
+			res.render('../views/cart/editorder.handlebars', { order });
+		})
+		.catch(err => console.log(err));
+});
+
+router.post('/editOrder/:id', ensureAuthenticated, (req, res) => {
+	let custno = req.user.custno;
+	let product = req.body.product;
+	let amount = req.body.amount;
+	let price = req.body.price;
+
+	Order.update(
+		{custno, product, amount, price},
+		{where: {id: req.params.id}}
+	)
+		.then((result) =>{
+			console.log('order' + '(' + result[0]+ ')' + 'updated');
+			res.redirect('../views/cart/cart.handlebars', { orders });
+		})
+
+});
 
 
 
