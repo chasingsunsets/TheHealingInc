325d4721e0ccebb6f14e3fd47dccd576f10e1967
Natthida <lnnathida@gmail.com>
2022-07-17 15:46:59 +0800

    newsletter email decor

--- routes/main.js
+++ routes/main.js
@@ -69,6 +69,7 @@ router.get('/booking', (req, res) => {
 
 // });
 
+// Newsletter Subscription
 router.get('/', (req, res) => {
 	res.render('newsletter/form', signUpPage);
 });
@@ -81,13 +82,112 @@ router.post('/', async (req, res) => {
 		email: req.body.email,
 	});
 	const confirmationURL = req.protocol + '://' + req.headers.host + '/confirm/?' + params;
+	const fname = req.body.firstName;
 	const msg = {
 		to: req.body.email,
 		from: 'thehealinginctester@gmail.com', // Change to your verified sender
 		subject: `Confirm your subscription to our newsletter`,
-		html: `Hello ${req.body.firstname},<br>Thank you for subscribing to our newsletter. Please complete and confirm your subscription by <a href="${confirmationURL}"> clicking here</a>.`
+		html: `
+		<body style="background-color: #f1ede5; color: black">
+		<br><br>
+		<!-- start preheader -->
+		<div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+			Confirm your subscription to our newsletter
+		</div>
+		<!-- end preheader -->
+
+		<!-- start body -->
+		<table border="0" cellpadding="0" cellspacing="0" width="100%">
+			<!-- start hero -->
+			<tr>
+			<td align="center" bgcolor="#f1ede5">
+				<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+				<tr>
+					<td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
+					<h1 style="text-align: centre; margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px; align="center>Just One Step Away!</h1>
+					</td>
+				</tr>
+				</table>
+			</td>
+			</tr>
+			<!-- end hero -->
+
+			<!-- start copy block -->
+			<tr>
+			<td align="center" bgcolor="#f1ede5">
+				<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+
+				<!-- start copy -->
+				<tr>
+					<td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+					<p style="margin: 0;">Dear ${fname}, <br><br>Thank you for subscribing to our newsletter. Tap the button below to confirm your email address for our newsletter subscription service. If you didn't create an account with <a href="localhost:5000">The Healing Inc</a>, you can safely delete this email.</p>
+					</td>
+				</tr>
+				<!-- end copy -->
+
+				<!-- start button -->
+				<tr>
+					<td align="left" bgcolor="#ffffff">
+					<table border="0" cellpadding="0" cellspacing="0" width="100%">
+						<tr>
+						<td align="center" bgcolor="#ffffff" style="padding: 12px;">
+							<table border="0" cellpadding="0" cellspacing="0">
+							<tr>
+								<td align="center" bgcolor="#8BAD97" style="border-radius: 6px;">
+								<a href="${confirmationURL}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Confirm Email</a>
+								</td>
+							</tr>
+							</table>
+						</td>
+						</tr>
+					</table>
+					</td>
+				</tr>
+				<!-- end button -->
+
+				<!-- start copy -->
+				<tr>
+					<td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+					<p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
+					<p style="margin: 0;"><a href="${confirmationURL}" target="_blank">link</a></p>
+					</td>
+				</tr>
+				<!-- end copy -->
+
+				<!-- start copy -->
+				<tr>
+					<td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
+					<p style="margin: 0;">Cheers,<br> The Healing Inc.</p>
+					</td>
+				</tr>
+				<!-- end copy -->
+
+				</table>
+			</td>
+			</tr>
+			<!-- end copy block -->
+
+			<!-- start footer -->
+			<tr>
+			<td align="center" bgcolor="#f1ede5" style="padding: 24px;">
+				<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+
+				<!-- start permission -->
+				<tr>
+					<td align="center" bgcolor="#f1ede5" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+					<p style="margin: 0;">You received this email because we received a request for newsletter subscription for your account. If you didn't request for newsletter subscription you can safely delete this email.</p>
+					</td>
+				</tr>
+				<!-- end permission -->
+				</table>
+			</td>
+			</tr>
+			<!-- end footer -->
+
+		</table>
+		`
 	}
-	await addContact(req.body.firstname, req.body.lastname, req.body.email, confNum);
+	await addContact(req.body.firstName, req.body.lastName, req.body.email, confNum);
 	await sgMail.send(msg);
 	res.render('newsletter/message', { message: 'Thank you for signing up for our newsletter! Please complete the process by confirming the subscription in your email inbox.' });
 });
