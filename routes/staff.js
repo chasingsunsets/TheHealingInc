76df3e607da7fff0362fcb2da74ea3870aebf12f
captaiN455 <q2467231710@gmail.com>
2022-08-16 16:29:15 +0800

    Update staff.js

--- routes/staff.js
+++ routes/staff.js
@@ -12,22 +12,22 @@ const { request } = require('express');
 
 router.get('/login', (req, res) => {
     try {
-    User.findOne({ where: { email: "thehealinginctester@gmail.com" } })
-        .then(staff => {
+        User.findOne({ where: { email: "thehealinginctester@gmail.com" } })
+            .then(staff => {
 
-            if (!staff) {
-                // Create main staff
-                var salt = bcrypt.genSaltSync(10);
-                var hash = bcrypt.hashSync("sssssss", salt);
-                // Use hashed password
-                User.create({ type:"staff", username: "staff", firstname: "John", lastname: "Tan", email: "thehealinginctester@gmail.com", password: hash });
-                console.log(' staff acc created');
+                if (!staff) {
+                    // Create main staff
+                    var salt = bcrypt.genSaltSync(10);
+                    var hash = bcrypt.hashSync("sssssss", salt);
+                    // Use hashed password
+                    User.create({ type: "staff", username: "staff", firstname: "John", lastname: "Tan", email: "thehealinginctester@gmail.com", password: hash });
+                    console.log(' staff acc created');
+                    res.render('./staff/login', { layout: 'stafflogin' });
+                    return;
+                }
                 res.render('./staff/login', { layout: 'stafflogin' });
-                return;
-            }
-            res.render('./staff/login', { layout: 'stafflogin' });
-            
-        });
+
+            });
     }
     catch (err) {
         console.log(err);
@@ -48,7 +48,7 @@ router.post('/login', (req, res, next) => {
 });
 
 router.get('/register', ensureAuthenticatedStaff, (req, res) => {
-    res.render('./staff/register', { layout: 'staffMain', username: req.username, id: req.id});
+    res.render('./staff/register', { layout: 'staffMain', username: req.username, id: req.id });
 });
 
 router.post('/register', ensureAuthenticatedStaff, async function (req, res) {
@@ -73,17 +73,17 @@ router.post('/register', ensureAuthenticatedStaff, async function (req, res) {
     }
 
     try {
-        
+
         // If all is well, checks if user is already registered
-        let staffe = await User.findOne({ where: { email: email, type:"staff"} });
-        let staffn = await User.findOne({ where: { username: username, type:"staff"} });
+        let staffe = await User.findOne({ where: { email: email, type: "staff" } });
+        let staffn = await User.findOne({ where: { username: username, type: "staff" } });
 
         if (staffe) {
             // If staff is found, that means email has already been registered
             flashMessage(res, 'error', email + ' already registered');
             res.render('./staff/register', {
                 layout: 'staffMain',
-                 username, firstname, lastname, email
+                username, firstname, lastname, email
             });
         }
 
@@ -92,7 +92,7 @@ router.post('/register', ensureAuthenticatedStaff, async function (req, res) {
             flashMessage(res, 'error', username + ' already registered');
             res.render('./staff/register', {
                 layout: 'staffMain',
-                 username, firstname, lastname, email
+                username, firstname, lastname, email
             });
         }
         else {
@@ -100,7 +100,7 @@ router.post('/register', ensureAuthenticatedStaff, async function (req, res) {
             var salt = bcrypt.genSaltSync(10);
             var hash = bcrypt.hashSync(password, salt);
             // Use hashed password
-            let staff = await User.create({ type:"staff", username, firstname, lastname, email, password: hash });
+            let staff = await User.create({ type: "staff", username, firstname, lastname, email, password: hash });
             flashMessage(res, 'success', username + ' registered successfully');
             res.redirect('/staff/dashboard');
         }
@@ -164,7 +164,7 @@ router.post('/editProfile/:id', ensureAuthenticatedStaff, async function (req, r
                 if (user.id != id) {
                     // If user is found, that means email has already been registered
                     flashMessage(res, 'error', email + ' already registered');
-                    isValid = false; 
+                    isValid = false;
                 }
 
             })
@@ -179,35 +179,35 @@ router.post('/editProfile/:id', ensureAuthenticatedStaff, async function (req, r
 
                 }
             })
-                .catch(err => console.log(err));
-                
-    if (!isValid) {
-        User.findByPk(req.params.id)
-            .then((user) => {
-                res.render('staff/editProfile', { layout: 'staffMain', user });
-            })
             .catch(err => console.log(err));
-        return;
-    }
+
+        if (!isValid) {
+            User.findByPk(req.params.id)
+                .then((user) => {
+                    res.render('staff/editProfile', { layout: 'staffMain', user });
+                })
+                .catch(err => console.log(err));
+            return;
+        }
     }
 
     catch (err) {
         console.log(err);
     }
 
-            User.update(
-            // { firstname, lastname, username, phoneno, address, email, password: hash },
-            { firstname, lastname, username, email },
-            { where: { id: req.params.id, type: "staff" } }
-        )
-            .then((result) => {
-                console.log(result[0] + ' profile updated');
-                flashMessage(res, 'success', ' Profile edited successfully');
-                res.redirect('/staff/profile');
-            })
-            .catch(err => console.log(err));
+    User.update(
+        // { firstname, lastname, username, phoneno, address, email, password: hash },
+        { firstname, lastname, username, email },
+        { where: { id: req.params.id, type: "staff" } }
+    )
+        .then((result) => {
+            console.log(result[0] + ' profile updated');
+            flashMessage(res, 'success', ' Profile edited successfully');
+            res.redirect('/staff/profile');
+        })
+        .catch(err => console.log(err));
 
-    });
+});
 
 ////////////////////////////////////////////////////
 
@@ -244,7 +244,7 @@ router.post('/changePW/:id', ensureAuthenticatedStaff, (req, res) => {
 
     let isValid = true;
 
-    User.findOne({ where: { username: req.user.username,type:"staff" } })
+    User.findOne({ where: { username: req.user.username, type: "staff" } })
         .then(user => {
             // Match password
             isMatch = bcrypt.compareSync(password, user.password);
@@ -269,7 +269,7 @@ router.post('/changePW/:id', ensureAuthenticatedStaff, (req, res) => {
             if (!isValid) {
                 User.findByPk(req.params.id)
                     .then((user) => {
-                        res.render('staff/changePW', {layout: 'staffMain', user});
+                        res.render('staff/changePW', { layout: 'staffMain', user });
                     })
                     .catch(err => console.log(err));
                 return;
@@ -339,7 +339,7 @@ router.get('/listCust', ensureAuthenticatedStaff, (req, res) => {
     // res.render('./staff/listCust', { layout: 'staffMain', user: req.user, firstname: req.user.firstname, lastname: req.user.lastname, username: req.user.username, phoneno: req.user.phoneno, address: req.user.address, email: req.user.email, id: req.user.id });
 });
 
-router.get('/listStaff',ensureAuthenticatedStaff, (req, res) => {
+router.get('/listStaff', ensureAuthenticatedStaff, (req, res) => {
     User.findAll({
         where: { type: "staff" },
         // order: [['dateRelease', 'DESC']],
@@ -374,7 +374,7 @@ router.get('/editCust/:id', ensureAuthenticatedStaff, (req, res) => {
         .catch(err => console.log(err));
 });
 
-router.post('/editCust/:id',ensureAuthenticatedStaff, (req, res) => {
+router.post('/editCust/:id', ensureAuthenticatedStaff, (req, res) => {
     let firstname = req.body.firstname;
     let lastname = req.body.lastname;
     let username = req.body.username;
@@ -445,7 +445,7 @@ router.get('/editStaff/:id', ensureAuthenticatedStaff, (req, res) => {
         .catch(err => console.log(err));
 });
 
-router.post('/editStaff/:id',ensureAuthenticatedStaff, (req, res) => {
+router.post('/editStaff/:id', ensureAuthenticatedStaff, (req, res) => {
     let staffno = req.body.staffno;
     let firstname = req.body.firstname;
     let lastname = req.body.lastname;
@@ -467,7 +467,7 @@ router.post('/editStaff/:id',ensureAuthenticatedStaff, (req, res) => {
         .catch(err => console.log(err));
 });
 
-router.get('/deletestaff/:id',ensureAuthenticatedStaff, async function (req, res) {
+router.get('/deletestaff/:id', ensureAuthenticatedStaff, async function (req, res) {
     try {
         let staff = await User.findByPk(req.params.id);
         if (!staff) {
@@ -498,21 +498,21 @@ router.get('/dashboard', ensureAuthenticatedStaff, (req, res) => {
 });
 
 
-router.get('/listOrderbyTime',ensureAuthenticatedStaff, async (req, res) => {
+router.get('/listOrderbyTime', ensureAuthenticatedStaff, async (req, res) => {
     const orders = await Order.Order.findAll({
         order: [['createdat', 'DESC']],
         raw: true
     })
-    res.render('./staff/listCustOrder', {layout: 'staffMain' ,orders});
+    res.render('./staff/listCustOrder', { layout: 'staffMain', orders });
 });
 
 
-router.post('/listOrderbyTime',ensureAuthenticatedStaff, async (req, res) => {
+router.post('/listOrderbyTime', ensureAuthenticatedStaff, async (req, res) => {
     const order = await Order.Order.findByPk(req.body.order_id);
     let status = req.body.status
     order.update(
         { status: status }
-        );
+    );
     res.redirect("/staff/listOrderbyTime");
 })
 
@@ -537,18 +537,18 @@ router.get('/deleteOrder/:id', async function (req, res) {
     }
 });
 
-router.get('/listOrderbyCustomer', async  (req, res) => {
+router.get('/listOrderbyCustomer', async (req, res) => {
     const users = await User.findAll({
-        where: {type: "customer"},
+        where: { type: "customer" },
         raw: true
     })
     console.log(users);
-    res.render('./staff/listOrderbyCustomer.handlebars', {layout: 'staffMain' ,users})
+    res.render('./staff/listOrderbyCustomer.handlebars', { layout: 'staffMain', users })
 });
 
 router.get('/manageCustOrder/:id', async (req, res) => {
     const orders = await Order.Order.findAll({
-        where: {userId: req.params.id},
+        where: { userId: req.params.id },
         raw: true
     });
     res.render('staff/manageCustOrder.handlebars', { layout: 'staffMain', orders });
@@ -559,8 +559,8 @@ router.post('/manageCustOrder/:id', async (req, res) => {
     let status = req.body.status
     order.update(
         { status: status }
-        );
-    res.redirect("/staff/manageCustOrder/"+ req.params.id);
+    );
+    res.redirect("/staff/manageCustOrder/" + req.params.id);
 });
 
 
@@ -568,22 +568,22 @@ router.get('/Addreason', ensureAuthenticatedStaff, (req, res) => {
     res.render('staff/addReason', { layout: 'staffMain' });
 });
 
-router.post('/Addreason', ensureAuthenticatedStaff,(req, res) => {
+router.post('/Addreason', ensureAuthenticatedStaff, (req, res) => {
     let summary = req.body.newsletterName;
     let category = req.body.category;
     let content = req.body.htmlContent;
     let createdBy = req.body.createdBy;
     Order.Reason.create({
-        summary, category,content,createdBy
+        summary, category, content, createdBy
     })
-    .then((reason) => {
-        console.log(reason);
-        res.redirect('/staff/listReasons');
-    })
+        .then((reason) => {
+            console.log(reason);
+            res.redirect('/staff/listReasons');
+        })
 });
 
 router.get('/listReasons', ensureAuthenticatedStaff, async (req, res) => {
-    let reasons = await Order.Reason.findAll({raw:true});
+    let reasons = await Order.Reason.findAll({ raw: true });
     res.render('staff/listReasons', { layout: 'staffMain', reasons });
 });
 
@@ -609,30 +609,48 @@ router.post('/editReason/:id', ensureAuthenticatedStaff, (req, res) => {
     let content = req.body.htmlContent;
     let createdBy = req.body.createdBy;
 
-            Order.Reason.update(
-            { summary, category, content, createdBy},
-            { where: { id:req.params.id } }
-        )
-            .then((result) => {
-                console.log(result[0] + ' reason updated');
-                flashMessage(res, 'success', ' Profile edited successfully');
-                res.redirect('/staff/listReasons');
-            })
+    Order.Reason.update(
+        { summary, category, content, createdBy },
+        { where: { id: req.params.id } }
+    )
+        .then((result) => {
+            console.log(result[0] + ' reason updated');
+            flashMessage(res, 'success', ' Profile edited successfully');
+            res.redirect('/staff/listReasons');
+        })
 
-    });
+});
 
-router.get('/selectReason/:id', ensureAuthenticatedStaff, async (req, res) =>{
-    let order = Order.Order.findByPk(req.params.id);
+router.get('/selectReason/:id', ensureAuthenticatedStaff, async (req, res) => {
+    let order = await Order.Order.findByPk(req.params.id);
     let user_id = order.userId;
 
-    let reasons = await Order.Reason.findAll({raw:true});
-    
-    res.render('staff/selectReason', {layout: 'staffMain', reasons, user_id: user_id}); 
+    let reasons = await Order.Reason.findAll({ raw: true });
+
+    res.render('staff/selectReason', { layout: 'staffMain', reasons, user_id: user_id });
 
 });
 
 router.get('/sendReasons/:id', ensureAuthenticatedStaff, async (req, res) => {
+    let user_id = req.body.user_id;
+    
+    let reason = await Order.Reason.findByPk(req.params.id);
+    let user = await User.findByPk(user_id);
+    console.log("reason:"+ reason);
+    console.log("user: " + user);
 
+//send email
+    let htmlContent = reason.content;
+    let summary = reason.summary;
+    let toEmail = user.email;
+    let firstName = user.firstName;
+    let lastName = user.lastName;
+
+    sendEmail2(toEmail, subject, firstName, lastName, htmlContent, unSubURL, attachment)
+    .then(response => {
+        flashMessage(res, 'success',"Email has been send successfully.")
+        res.redirect('staff/listOrderbyTime')
+    })
 })
 
 module.exports = router;
